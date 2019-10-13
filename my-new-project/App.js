65de8dd150ef8b5cb8daf214e62e8f46980b765a
Alan Lyne <alanlyne@outlook.com>
2019-10-13 21:49:27 +0100

    slight update

--- my-new-project/App.js
+++ my-new-project/App.js
@@ -4,9 +4,9 @@ import { StyleSheet, Text, View, TextInput, Button} from 'react-native';
 export default function App() {
   return (
     <View style={{padding: 50}}>
-      <View>
+      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
         <TextInput placeholder="Enter your goal" 
-        style={{borderColor: 'black', borderWidth: 1}}
+        style={{borderColor: 'black', borderWidth: 1, padding: 10}}
         />
         <Button title="ADD" />
       </View>
