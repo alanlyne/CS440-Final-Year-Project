3666d2135cbd49a581d9c0bff9fe9c518d2cf282
Alan Lyne <alanlyne@outlook.com>
2019-10-13 21:38:53 +0100

    First Commit

---
+++ my-new-project/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button} from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{padding: 50}}>
+      <View>
+        <TextInput placeholder="Enter your goal" 
+        style={{borderColor: 'black', borderWidth: 1}}
+        />
+        <Button title="ADD" />
+      </View>
+      <View>
+
+      </View>
+    </View>
+  );
+} 
+
+const styles = StyleSheet.create({
+  
+});
