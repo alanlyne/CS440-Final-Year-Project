b11e17018db35fd005580ad2daa7b4ab89546c1c
Alan Lyne <alanlyne@outlook.com>
2019-10-16 16:27:11 +0100

    16/10

--- my-new-project/App.js
+++ my-new-project/App.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button} from 'react-native';
 
+
+//Input Box with Button
+
 export default function App() {
   return (
     <View style={{padding: 50}}>
       <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
-        <TextInput placeholder="Enter your goal" 
-        style={{borderColor: 'black', borderWidth: 1, padding: 10}}
+        <TextInput 
+        placeholder="Enter your goal" 
+        style={{ width: '80%', borderColor: 'black', borderWidth: 1, padding: 10}}
         />
         <Button title="ADD" />
       </View>
@@ -15,8 +19,56 @@ export default function App() {
       </View>
     </View>
   );
-} 
+}
 
-const styles = StyleSheet.create({
-  
-});
+
+
+/* Boxes & Sizing
+export default function App() {
+  return (
+    <View style={{
+      padding: 50,  
+      flexDirection: 'row', 
+      width: '80%', 
+      height: 300, 
+      justifyContent: 'space-around', 
+      alignItems: 'stretch'
+      }}
+    >
+
+      <View
+        style={{
+          backgroundColor: 'red',
+          flex: 3,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Text>1</Text>
+      </View>
+
+      <View
+        style={{
+          backgroundColor: 'blue',
+          flex: 2,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Text>2</Text>
+      </View>
+
+      <View
+        style={{
+          backgroundColor: 'green',
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Text>3</Text>
+      </View>
+
+    </View>
+
+  );
+} */
