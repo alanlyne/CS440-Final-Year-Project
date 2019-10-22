349d731dcd51bfc65c3f24d2c01991d437d55441
Alan Lyne <alanlyne@outlook.com>
2019-10-22 14:16:06 +0100

    Added Scrolling

--- my-new-project/App.js
+++ my-new-project/App.js
@@ -1,37 +1,88 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button} from 'react-native';
-
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  ScrollView,
+  FlatList
+} from "react-native";
 
 //Input Box with Button
 
 export default function App() {
-  return (
-    <View style={{padding: 50}}>
-      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
-        <TextInput 
-        placeholder="Enter your goal" 
-        style={{ width: '80%', borderColor: 'black', borderWidth: 1, padding: 10}}
-        />
-        <Button title="ADD" />
-      </View>
-      <View>
+  const [enteredGoal, setEnteredGoal] = useState("");
+  const [goals, setGoals] = useState([]);
 
+  const goalInputHandler = enteredText => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    setGoals(currentGoals => [
+      ...currentGoals,
+      { id: Math.random().toString(), value: enteredGoal }
+    ]);
+  };
+
+  return (
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Enter your goal"
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button title="ADD" onPress={addGoalHandler} />
       </View>
+      <FlatList
+        keyExtractor={(item, index) => item.id}
+        data={goals}
+        renderItem={itemData => (
+          <View style={styles.listItem}>
+            <Text>{itemData.item.value}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
 
-
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  input: {
+    width: "80%",
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: "#ccc",
+    borderColor: "black",
+    borderWidth: 1
+  }
+});
 
 /* Boxes & Sizing
 export default function App() {
   return (
     <View style={{
-      padding: 50,  
-      flexDirection: 'row', 
-      width: '80%', 
-      height: 300, 
-      justifyContent: 'space-around', 
+      padding: 50,
+      flexDirection: 'row',
+      width: '80%',
+      height: 300,
+      justifyContent: 'space-around',
       alignItems: 'stretch'
       }}
     >
