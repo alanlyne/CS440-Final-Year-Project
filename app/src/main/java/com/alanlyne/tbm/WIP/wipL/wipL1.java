58e38c627c36c520f59fa79e2bf415ad859549af
Alan Lyne <alanlyne@outlook.com>
2019-12-02 21:03:22 +0000

    Text is displayed on button press

--- app/src/main/java/com/alanlyne/tbm/WIP/wipL/wipL1.java
+++ app/src/main/java/com/alanlyne/tbm/WIP/wipL/wipL1.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.alanlyne.tbm.Menu.Menu;
 import com.alanlyne.tbm.Menu.menuCounter;
@@ -15,10 +17,11 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class wipL1 extends AppCompatActivity {
+public class wipL1 extends AppCompatActivity{
 
-    Button btn;
-
+    Button btn, c1;
+    TextView t1;
+    int counter = 0;
     DatabaseReference databaseName;
 
     @Override
@@ -29,7 +32,28 @@ public class wipL1 extends AppCompatActivity {
         databaseName = FirebaseDatabase.getInstance().getReference("counter");
 
 
+
         btn = findViewById(R.id.finishL1);
+        c1 = findViewById(R.id.c1);
+        t1 = findViewById(R.id.t1);
+
+
+
+
+        c1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter = 1;
+                System.out.println(counter);
+
+                if(counter == 1){
+                    TextView tv = findViewById(R.id.t1);
+                    tv.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
