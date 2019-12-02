7c4493bf45f21ddd76aba017fe6a2a574e92f081
alanlyne <alanlyne@outlook.com>
2019-12-02 17:55:18 +0000

    Changed background image

--- app/src/main/java/com/alanlyne/tbm/Auth/MainActivity.java
+++ app/src/main/java/com/alanlyne/tbm/Auth/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnLogin = findViewById(R.id.logout);
+        //btnLogin = findViewById(R.id.logout);
         tvSignIn = findViewById(R.id.login);
 
         // Move to Login
